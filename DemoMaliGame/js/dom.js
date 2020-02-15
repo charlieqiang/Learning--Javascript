be7c6e888aa1869b6da1d96d2ecf770265bb1d37
chenlinqiang <327371999@qq.com>
2020-02-15 23:25:14 +0800

    js bom and dom

--- DemoMaliGame/js/dom.js
+++ DemoMaliGame/js/dom.js
@@ -23,5 +23,76 @@ function reRun(){
 function inter(){
     var newWindow=window.open("newWindow.html","_blank");
 }
+function checkU(){
+    if($('uname').nodeValue()=="char"&&$("pwd").nodeValue()=="123"){
+        alert('ok');
+        return true;
 
-setTimeout("deletew()",5000);
+    }else{
+        $('uname').nodeValue()="";
+        alert('ono');
+        return false;
+    }
+}
+function $(id){
+    return document.getElementById(id);
+}
+function checkH(){
+    var hobbies=document.getElementsByName("hobby");
+    for(var i=0;i<hobbies.length;i++){
+        if(hobbies[i].checked)
+        {
+            alert("ok");
+            alert("your hobby is"+hobbies[i].value);
+        }
+    }
+}
+function findTag(){
+    var myObjs=document.getElementsByTagName("input");
+    for(var i=0;i<myObjs.length;i++){
+        window.alert(myObjs[i].value);
+    }
+}
+function god(){
+    var myElement=document.createElement("input");
+    myElement.value="god";
+    myElement.type="button";
+    myElement.id="Adam"
+    // myElement.href="http://www.spe.kim";
+    // myElement.innerText="go to spe";
+    document.getElementById("world").appendChild(myElement);
+}
+function godD(){
+    // document.getElementById("world").removeChild(document.getElementById("Adam"));
+    //flexble
+    document.getElementById("Adam").parentNode.removeChild(document.getElementById("Adam"));
+}
+//sun raise sun down
+directX=1;
+directY=1;
+var mySun=document.getElementById('sun');  
+var sunTop=mySun.style.top; 
+var sunLeft=mySun.style.left; 
+sunX=10;
+sunY=10;
+
+var divG=document.getElementById("cssDriver");
+
+
+function sunMove(){
+    
+    sunX+=directX;
+    sunY+=directY;
+    //change
+    sun.style.top=sunY+"px";
+    sun.style.left=sunX+"px";
+    if(sunX+sun.offsetWidth>=divG.clientWidth+10||sunX<=9){
+        directX=-directX;
+    }
+    if(sunY+sun.offsetHeight>=divG.clientHeight+10||sunY<=9){
+        directY=-directY;
+    }
+}
+// window.alert(sunTop.substr(0,sunTop.length-2));  
+setInterval("sunMove()",10);
+// setTimeout("deletew()",5000);
